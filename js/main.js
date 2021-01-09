5bf32d8b203fa6d4b80ecb471250559630be6eda
ATM <naik.atmaram@gmail.com>
2021-01-09 16:49:36 +0530

    ATM | changed styles

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 $(function () {
     $(document).scroll(function () {
         var $nav = $(".fixed-top");
+        $nav.toggleClass('navbar-dark', $(this).scrollTop() > 0 );
         $nav.toggleClass('bg-primary', $(this).scrollTop() > 0 );
-        $nav.toggleClass('bg-transparent', $(this).scrollTop() == 0 );
+        $nav.toggleClass('navbar-transparent', $(this).scrollTop() == 0 );
     });
 });
